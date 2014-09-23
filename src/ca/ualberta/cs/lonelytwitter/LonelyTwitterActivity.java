e8cfb8e78553825f1fd83143ebd64cdefc868bdb
iameric1986 <ec10@ualberta.ca>
2014-09-22 19:45:23 -0600

    Has a counter class

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,12 +6,20 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Date;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,13 +31,16 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	static ArrayList<LonelyTweetModel> tweets;
+	private ArrayAdapter<LonelyTweetModel> adapter;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		
+		// Places pointers to the widgets that are in the resources
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
@@ -39,34 +50,49 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
-
+				tweets.add(new LonelyTweetModel(text));
+				adapter.notifyDataSetChanged();
+				saveInFile();
 			}
 		});
+	}
+	
+	public ArrayList<LonelyTweetModel> getArray() {
+		return tweets;
 	}
 
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		loadFromFile();
+		if (tweets == null) {
+			tweets = new ArrayList<LonelyTweetModel>();
+		}
+		adapter = new ArrayAdapter<LonelyTweetModel>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void getCounter(MenuItem menu) {
+		Intent counter = new Intent(this, CounterActivity.class);
+		startActivity(counter);
+	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
-
+			InputStreamReader isr = new InputStreamReader(fis); //Json wants an InputStreamReader NOT a FileInputStream
+			//from http://www.javacreed.com/simple-gson-example/ accessed 09/22/2014
+			Gson gson = new GsonBuilder().create();
+			tweets = gson.fromJson(isr, new TypeToken<ArrayList<LonelyTweetModel>>() {}.getType()); //Use Json for assignment
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -74,16 +100,18 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
 	}
 	
-	private void saveInFile(String text, Date date) {
+	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
+			FileOutputStream fos = openFileOutput(FILENAME, 0); //Two ways to write to a file -- open for writing or open for appending.  Writing will erase anything in the file;
+										  						//Append will let you write at the end of the file.
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			//from http://www.javacreed.com/simple-gson-example/ accessed 09/22/2014
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(tweets, osw);
+			osw.flush();
+			osw.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
